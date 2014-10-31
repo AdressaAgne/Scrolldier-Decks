34dce1075ac9efe43e2e926958b716041db40004
GrygrFlzr <grygrflzr@hotmail.com>
2014-10-31 21:48:01 +0800

    Refactored get_deck_data()
    Uses new DeckData and Scroll classes
    Removed SQL query from loop

--- controllers/deckController.php
+++ controllers/deckController.php
@@ -63,233 +63,413 @@ class Deck extends Database {
 		
 	}
 	
+	/**
+	 * Retrieves the faction of a scroll
+	 * @param array $scroll
+	 * @return string Returns the faction of the scroll
+	 */
+	function getFaction($scroll)
+	{
+		if ($scroll['costgrowth'] > 0)
+		{
+			return 'growth';
+		}
+		elseif ($scroll['costorder'] > 0)
+		{
+			return 'order';
+		}
+		elseif ($scroll['costenergy'] > 0)
+		{
+			return 'energy';
+		}
+		elseif ($scroll['costdecay'] > 0)
+		{
+			return 'decay';
+		}
+	}
 	
-	public function get_deck_data($id) {
-		
-		
+	/**
+	 * Gets the cost of the scroll stack
+	 * @param Scroll $scroll
+	 * @return int The total price of the stack
+	 */
+	function getStackCost($scroll)
+	{
+		$cost = 100;
+		if ($scroll->rarity == 1)
+		{
+			$cost = 500;
+		}
+		elseif ($scroll->rarity == 2)
+		{
+			$cost = 1000;
+		}
+		return $cost * $scroll->count;
+	}
+	
+	
+	/**
+	 * Retrieves a deck
+	 * @param int $id The ID of the deck to retrieve
+	 * @return DeckData|boolean Returns a deck if the specified ID exists, FALSE otherwise
+	 */
+	public function get_deck_data($id)
+	{
+		// Get deck from database
 		$query = $this->_db->prepare("SELECT * FROM decks WHERE id = :id");
-		$query_arr = array(
-				'id' => $id
-			);
-		$this->arrayBinderInt($query, $query_arr);	
+		$arr = array(
+		    'id' => $id
+		);
+		$this->arrayBinderInt($query, $arr);
 		$query->execute();	
 		$data = $query->fetch(PDO::FETCH_ASSOC);
 		
-		$deck_data = array(
-			"name" => $data['deck_title'],
-			"author" => $data['deck_author'],
-			"meta" => $data['meta'],
-			"scrolls" => 0,
-			"id" => $data['id'],
-			"time" => $this->ago($data['time'])." ago",
-			"raw_time" => $data['time'],
-			"vote_up" => $data['vote'],
-			"vote_down" => $data['vote_down'],
-			"scrolls" => "",
-			"cost" => 0,
-			"image" => "decay-1",
-			"ressourses" => array(),
-			"percentage" => array(),
-			"types" => array(),
-//			"traits" => array(),
-			"subtype" => array(),
-			"rarities" => array(
-				0 => 0,
-				1 => 0,
-				2 => 0
-			),
-			"curve" => array(
-			
-			),
-			"sets" => array(
-				1 => 0,
-				2 => 0,
-				3 => 0,
-				4 => 0,
-				5 => 0,
-				6 => 0,
-				7 => 0
-			),
-			"attack" => array(),
-			"countdown" => array(),
-			"health" => array(),
-			"scrolls_values" => array(),
-			"text" => ""
-		);
-		
-		
-		//cost: Budget, Normal, Expensive
+		// Check deck exists
+		if (empty($data))
+		{
+			return FALSE;
+		}
 		
 		$json = json_decode($data['JSON'], TRUE);
 		
-		if ($json['msg'] == "success") {
-			for ($i = 0; $i < count($json['data']['scrolls']); $i++) {
-				
-				$scroll_id = $json['data']['scrolls'][$i]['id'];
-				$scroll_count = $json['data']['scrolls'][$i]['c'];
-				
-				$query = $this->_db->prepare("SELECT * FROM scrollsCard WHERE id = :id");
-				$arr = array(
-						'id' => $scroll_id
-					);
-				
-				$this->arrayBinderInt($query, $arr);
-				$query->execute();		
-				$scroll = $query->fetch(PDO::FETCH_ASSOC);
-				
-				
-				
-				$thisScrollFaction = "";
-				
-				if (!empty($scroll['costgrowth'])) {
-					
-				
-					$deck_data['ressourses']["growth"] += $scroll_count;
-					$deck_data['curve']['growth']['all'][$scroll['costgrowth']] += $scroll_count;
-					
-					$thisScrollFaction = "growth";
-					ksort($deck_data['curve']['growth']['all']);
-					
-					$deck_data['curve']['growth'][$scroll['kind']][$scroll['costgrowth']] += $scroll_count;
-				}
-				if (!empty($scroll['costorder'])) {
-					
-				
-					$deck_data['ressourses']["order"] += $scroll_count;
-					$deck_data['curve']['order']['all'][$scroll['costorder']] += $scroll_count;
-					$deck_data['curve']['order'][$scroll['kind']][$scroll['costorder']] += $scroll_count;
-					
-					$thisScrollFaction = "order";
-					ksort($deck_data['curve']['order']['all']);
-				}
-				if (!empty($scroll['costenergy'])) {
-					
-				
-					$deck_data['ressourses']["energy"]+= $scroll_count;
-					$deck_data['curve']['energy']['all'][$scroll['costenergy']] += $scroll_count;
-					
-					$deck_data['curve']['energy'][$scroll['kind']][$scroll['costenergy']] += $scroll_count;
-					$thisScrollFaction = "energy";
-					ksort($deck_data['curve']['energy']['all']);
-				}
-				if (!empty($scroll['costdecay'])) {
-					
-					
-				
-					$deck_data['ressourses']["decay"]+= $scroll_count;
-					$deck_data['curve']['decay']['all'][$scroll['costdecay']] += $scroll_count;
-					
-					$deck_data['curve']['decay'][$scroll['kind']][$scroll['costdecay']] += $scroll_count;
-					$thisScrollFaction = "decay";
-					ksort($deck_data['curve']['decay']['all']);
-				}
-				
-				$deck_data['types'][$scroll['kind']] += $scroll_count;
-				
-				$thisScrolls = array(
-					"id" => $scroll['id'],
-					"name" => $scroll['name'],
-					"count" => $scroll_count,
-					"rarity" => $scroll['rarity'],
-					"ressours" => $thisScrollFaction,
-					"image" => $scroll['image']
-				);
-				
-				array_push($deck_data['scrolls_values'], $thisScrolls);
-				
-				
-				$deck_data['rarities'][$scroll['rarity']] += $scroll_count;
-				
-				if ($scroll['kind'] == "CREATURE" || $scroll['kind'] == "STRUCTURE") {
-					$deck_data['attack'][$scroll['ap']] += $scroll_count;
-					$deck_data['countdown'][$scroll['ac']] += $scroll_count;
-					$deck_data['health'][$scroll['hp']] += $scroll_count;
-				}
-				
-				ksort($deck_data['attack']);
-				ksort($deck_data['countdown']);
-				ksort($deck_data['health']);
-				
-				switch ($scroll['rarity']) {
-					
-					case 0:
-						$deck_data['cost'] += 100 * $scroll_count;
-					break;
-					case 1:
-						$deck_data['cost'] += 500 * $scroll_count;
-					break;
-					case 2:
-						$deck_data['cost'] += 1000 * $scroll_count;
-					break;
-				}
-				
-				$deck_data['sets'][$scroll['scrollsSet']] += $scroll_count;
-
-				$sub_types = explode(",", $scroll['types']);
-				
-				
-				foreach ($sub_types as $key => $value) {
-					if (empty($value)) {
-						$deck_data['subtype']['none'] += $scroll_count;
-					} else {
-						$deck_data['subtype'][$value] += $scroll_count;
-					}
-				}
-				
-				arsort($deck_data['subtype']);
-				
-//				if (!empty($scroll['passiverules_1'])) {
-//					$deck_data['traits'][$scroll['passiverules_1']] += $scroll_count;
-//				}
-//				if (!empty($scroll['passiverules_2'])) {
-//					$deck_data['traits'][$scroll['passiverules_2']] += $scroll_count;
-//				}
-//				if (!empty($scroll['passiverules_3'])) {
-//					$deck_data['traits'][$scroll['passiverules_3']] += $scroll_count;
-//				}
-				
-				
-			
-				
-				$deck_data['scrolls'] += $scroll_count;
-			}
-			
-			$maxs = array_keys($deck_data['ressourses'], max($deck_data['ressourses']));
-			
-			$deck_data['image'] = $maxs[0]."-".rand(1, 4).".jpg";
-			
-			$total = 0;
-			
-			foreach ($deck_data['ressourses'] as $key => $value) {
-				$total += $deck_data['ressourses'][$key];
-			}
-			
-			foreach ($deck_data['ressourses'] as $key => $value) {
-				$deck_data['percentage'][$key] = intval(($deck_data['ressourses'][$key] / $total) * 100);
-			}
-			
-			foreach ($deck_data['curve'] as $faction => $value) {
-			
-				foreach ($deck_data['curve'][$faction] as $type => $type_valu) {
-					for ($i = 1; $i < 10; $i++) {
-						if (!isset($deck_data['curve'][$faction][$type][$i])) {
-							$deck_data['curve'][$faction][$type][$i] = 0;
-						}
-					}
-					
-					ksort($deck_data['curve'][$faction][$type]);
-				}
-			}
-			
-			$deck_data['text'] = $data['text'];
-			//$deck_data['sets'] = array_values($deck_data['sets']);
-		} else {
-			return "No Deck with this ID";
+		// Set metadata
+		$deck_data = new DeckData();
+		$deck_data->id           = $id;
+		$deck_data->name         = $data['deck_title'];
+		$deck_data->author       = $data['deck_author'];
+		$deck_data->text         = $data['text'];
+		$deck_data->meta_version = $data['meta'];
+		$deck_data->time         = $this->ago($data['time']) . ' ago';
+		$deck_data->vote_up      = $data['vote'];
+		$deck_data->vote_down    = $data['vote_down'];
+		
+		// Parse scroll IDs
+		$scroll_ids = [];
+		$scrolls_count = [];
+		foreach ($json['data']['scrolls'] as $scroll)
+		{
+			$scroll_ids[] = $scroll['id'];
+			$scrolls_count[$scroll['id']] = $scroll['c'];
 		}
 		
-		return $deck_data;
+		// Retrieve scrolls
+		$prepare = implode(',', array_fill(0, count($scroll_ids), '?'));
+		$query = $this->_db->prepare("SELECT * FROM scrollsCard WHERE id IN ($prepare)");
+		$query->execute($scroll_ids);
 		
+		// Populate deck with scrolls
+		$scrolls_data = $query->fetchAll();
+		$total = 0;
+		foreach ($scrolls_data as $scroll_data)
+		{
+			$scroll          = new Scroll();
+			$scroll->id      = $scroll_data['id'];
+			$scroll->name    = $scroll_data['name'];
+			$scroll->count   = $scrolls_count[$scroll->id];
+			$scroll->rarity  = $scroll_data['rarity'];
+			$scroll->faction = $this->getFaction($scroll_data);
+			$scroll->image   = $scroll_data['image'];
+			
+			$faction = $scroll->faction;
+			$cost = $scroll_data["cost$faction"];
+			$kind = $scroll_data['kind'];
+			
+			$deck_data->scrolls[]     = $scroll;
+			$deck_data->scroll_count += $scroll->count;
+			$deck_data->total_cost   += $this->getStackCost($scroll);
+			
+			$deck_data->addKind($kind, $scroll->count);
+			$deck_data->addRarity($scroll->rarity, $scroll->count);
+			$deck_data->addResource($faction, $scroll->count);
+			
+			$types = explode(',',$scroll_data['types']);
+			foreach ($types as $type)
+			{
+				if ($type === '')
+				{
+					$type = 'None';
+				}
+				$deck_data->addType($type, $scroll->count);
+			}
+			
+			$deck_data->addCurve($faction, 'all', $cost, $scroll->count);
+			$deck_data->addCurve($faction, $kind, $cost, $scroll->count);
+			
+			$total += $scroll->count;
+		}
+		
+		// Sort deck fields
+		ksort($deck_data->curve);
+		arsort($deck_data->types);
+		
+		// Calculate faction percentage
+		foreach ($deck_data->resources as $faction => $count)
+		{
+			if ($count > 0)
+			{
+				$deck_data->percentage[$faction] = $count * 100 / $total;
+			}
+		}
+		
+		// Set image from most used faction
+		arsort($deck_data->percentage);
+		$most_used = array_keys($deck_data->percentage);
+		$rand      = rand(1,4);
+		
+		$deck_data->image = $most_used[0] . "-$rand.jpg";
+		
+		return $deck_data;
 	}
 	
 	
 }
+
+/**
+ * A deck containing multiple scrolls
+ */
+class DeckData
+{
+	/**
+	 * The deck ID number
+	 * @var int
+	 */
+	public $id = 0;
+	/**
+	 * The name of the deck
+	 * @var string
+	 */
+	public $name = '';
+	/**
+	 * Author of the deck
+	 * @var string
+	 */
+	public $author = '';
+	/**
+	 * Description of the deck
+	 * @var string 
+	 */
+	public $text = '';
+	/**
+	 * The meta version of the deck
+	 * @var string
+	 */
+	public $meta_version = '';
+	/**
+	 * The filename of the cover image.<br>
+	 * Determined by most used faction and a random integer between 1 and 4.
+	 * @var string
+	 */
+	public $image = '';
+	/**
+	 * Creation time of the deck in human readable format
+	 * @var string
+	 */
+	public $time = '';
+	/**
+	 * Faction usage in percentages
+	 * @var int[] 
+	 */
+	public $percentage = [];
+	
+	/**
+	 * The amount of scrolls in the deck
+	 * @var int
+	 */
+	public $scroll_count = 0;
+	/**
+	 * The total cost of the deck
+	 * @var int
+	 */
+	public $total_cost = 0;
+	/**
+	 * The number of upvotes
+	 * @var int
+	 */
+	public $vote_up = 0;
+	/**
+	 * The number of downvotes
+	 * @var int
+	 */
+	public $vote_down = 0;
+	
+	/**
+	 * The amount of each kind<br>
+	 * Possible kinds: CREATURE, ENCHANTMENT, SPELL, STRUCTURE
+	 * @var int[]
+	 */
+	public $kinds = array(
+	    'CREATURE'    => 0,
+	    'ENCHANTMENT' => 0,
+	    'SPELL'       => 0,
+	    'STRUCTURE'   => 0
+	);
+	/**
+	 * The amount of each type<br>
+	 * Examples: None, Artillery, Automation, Destruction, Human, Masked
+	 * @var int[]
+	 */
+	public $types = [];
+	/**
+	 * The amount of each rarity
+	 * @var int[] 
+	 */
+	public $rarities = array(
+	    0 => 0,
+	    1 => 0,
+	    2 => 0
+	);
+	/**
+	 * The amount of each resource
+	 * @var int[]
+	 */
+	public $resources = [];
+	/**
+	 * The curve for each faction
+	 * @var array[]
+	 */
+	public $curve = array(
+	    'growth' => [],
+	    'order'  => [],
+	    'energy' => [],
+	    'decay'  => []
+	);
+	
+	/**
+	 * An array of scrolls
+	 * @var Scroll[]
+	 */
+	public $scrolls = [];
+	
+	/**
+	 * 
+	 * @param string $type
+	 * @param int $count
+	 */
+	public function addType($type, $count)
+	{
+		if( ! isset($this->types[$type]) )
+		{
+			$this->types[$type] = 0;
+		}
+		$this->types[$type] += $count;
+	}
+	
+	/**
+	 * 
+	 * @param string $kind
+	 * @param int $count
+	 */
+	public function addKind($kind, $count)
+	{
+		if( ! isset($this->kinds[$kind]) )
+		{
+			$this->kinds[$kind] = 0;
+		}
+		$this->kinds[$kind] += $count;
+	}
+	
+	/**
+	 * 
+	 * @param string $rarity
+	 * @param int $count
+	 */
+	public function addRarity($rarity, $count)
+	{
+		if( ! isset($this->rarities[$rarity]) )
+		{
+			$this->rarities[$rarity] = 0;
+		}
+		$this->rarities[$rarity] += $count;
+	}
+	
+	/**
+	 * 
+	 * @param string $resource
+	 * @param int $count
+	 */
+	public function addResource($resource, $count)
+	{
+		if( ! isset($this->resources[$resource]) )
+		{
+			$this->resources[$resource] = 0;
+		}
+		$this->resources[$resource] += $count;
+	}
+	
+	/**
+	 * Make sure the curve for a kind is available to increment
+	 * @param string $faction
+	 * @param string $kind
+	 */
+	public function initCurve($faction, $kind)
+	{
+		if (isset($this->curve[$faction][$kind]))
+		{
+			return;
+		}
+		for ($i=1; $i < 10; $i++)
+		{
+			$this->curve[$faction][$kind][$i] = 0;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param string $faction
+	 * @param string $kind
+	 * @param int $cost
+	 * @param int $count
+	 */
+	public function addCurve($faction, $kind, $cost, $count)
+	{
+		$this->initCurve($faction, $kind);
+		$this->curve[$faction][$kind][$cost] += $count;
+	}
+	
+	/**
+	 * Supresses warnings about non-object<br>
+	 * Unsure why this only happens with view-deck.php
+	 */
+	public function supressWarnings() {}
+}
+
+/**
+ * Scroll with extra metadata
+ */
+class Scroll
+{
+	/**
+	 * The ID of the scroll
+	 * @var int
+	 */
+	public $id = 0;
+	/**
+	 * The name of the scroll
+	 * @var string
+	 */
+	public $name = '';
+	/**
+	 * The amount of times the scroll appears in a deck
+	 * @var id
+	 */
+	public $count = 0;
+	/**
+	 * The rarity level of the scroll.<br>
+	 * 0:  100g<br>
+	 * 1:  500g<br>
+	 * 2: 1000g
+	 * @var int
+	 */
+	public $rarity = 0;
+	/**
+	 * The faction name of the scroll
+	 * @var string
+	 */
+	public $faction = '';
+	/**
+	 * The image name
+	 * @var string 
+	 */
+	public $image = '';
+}
+
--- view/view_deck.php
+++ view/view_deck.php
@@ -2,165 +2,154 @@
 
 $data = $deck->get_deck_data($base->get_var(1));
 
- ?>
- <div class="deck-bar">
- 	<?php foreach ($data['percentage'] as $key => $value) {
- 		echo('<div class="bar bar-'.$key.'" style="width: '.$value.'%;"></div>');
- 	} ?>
- </div>
+@$data->supressWarnings();
+
+?>
+<div class="deck-bar">
+	<?php
+	foreach ($data->percentage as $faction => $percent) {
+		echo '<div class="bar bar-'.$faction.'" style="width: '.$percent.'%;"></div>';
+	}
+	?>
+</div>
  
 <div class="container">
 
 	<div class="page-header">
 		<h2>
-		<?php 
-			foreach ($data['ressourses'] as $key => $value) {
-				echo('<i class="icon-gold-'.$key.' front-text"></i>');
+		<?php
+			foreach ($data->resources as $resource => $count) {
+				echo '<i class="icon-gold-'.$resource.' front-text"></i>';
 			}
 		?>
-		<?php echo($data["name"]) ?> <br />
-		<small>Score: <?php echo($data['vote_up'] - $data['vote_down']) ?> - by: <?php echo($data['author']." - ".$data['time']) ?></small></h2>
+		<?=$data->name ?> <br />
+		<small>Score: <?=($data->vote_up - $data->vote_down) ?> - by <?=$data->author ?> - <?=$data->time ?></small></h2>
 	</div>
 	
-
-		<div class="col-12 align-center">
-			
-			<button class="btn big success">Vote Up</button>
-			<button class="btn big danger">Vote Down</button>
-		</div>
+	<div class="col-12 align-center">
+		<button class="btn big success">Vote Up</button>
+		<button class="btn big danger">Vote Down</button>
+	</div>
 	
 	<div class="col-12">
-		
 		<div class="col-12">
-					<div class="col-12 deck-scrolls">
-						<?php 
-						
-		//					[id] => 21
-		//		            [name] => Rallying
-		//		            [count] => 3
-		//		            [rarity] => 1
-		//		            [ressours] => growth
-		//		            [image] => 490
-		
-							foreach ($data['scrolls_values'] as $key => $value) { ?>
-								
-								<div class="col-2 col-p-6 col-t-3" data-id="<?php echo($data['scrolls_values'][$key]['id']) ?>" data-count="<?php echo($data['scrolls_values'][$key]['count']) ?>">
-									 <div class="col-12 scroll scroll-stack-<?=$data['scrolls_values'][$key]['count']?>" style="background-image: url('/img/scrolls/<?php echo($data['scrolls_values'][$key]['image']) ?>.png');">
-									
-										<i class="icon-<?php echo($data['scrolls_values'][$key]['ressours']) ?>"></i>
-									</div>
-									<div class="col-12 scroll-content">
-										<?php echo("x".$data['scrolls_values'][$key]['count']) ?> <?php echo($data['scrolls_values'][$key]['name']) ?>
-									</div>
-								</div>
-								
-							<?php } ?>
+			<div class="col-12 deck-scrolls">
+				<?php foreach ($data->scrolls as $scroll) { ?>
+					<div class="col-2 col-p-6 col-t-3" data-id="<?=$scroll->id ?>" data-count="<?=$scroll->count ?>">
+						<div class="col-12 scroll scroll-stack-<?=$scroll->count ?>" style="background-image: url('/img/scrolls/<?=$scroll->image ?>.png');">
+							<i class="icon-<?=$scroll->faction ?>"></i>
+						</div>
+						<div class="col-12 scroll-content">
+							x<?=$scroll->count ?> <?=$scroll->name ?>
+						</div>
 					</div>
-				</div>
+				<?php } ?>
+			</div>
+		</div>
 		
 		<div class="col-12 col-t-12">
 				
-					<div class="page-header">
-						<h3>Statistics <small>General</small></h3>
-					</div>	
-					
-					<div class="col-12">
-					<div class="col-12">
-						
-					<?php 
-						foreach ($data['curve'] as $faction => $value) {
-							foreach ($data['curve'][$faction] as $key => $value) { ?>
-								<label class="btn small">
-									<input type="radio" name="curve" value="barChartData<?php echo($key) ?>" data="<?php echo($key) ?>" /> <?php echo(ucfirst(strtolower($key))) ?>
-								</label>
-							<?php }
-						} 
-					?>
-					</div>
-										
-						<div class="col-4 col-t-12">
-							<h4 class="list-stats">Curve</h4>
-							<div class="curveContainer">
-								<canvas id="curve-all"></canvas>
-								<canvas id="curve-SPELL" style="display: none;"></canvas>
-								<canvas id="curve-CREATURE" style="display: none;"></canvas>
-								<canvas id="curve-STRUCTURE" style="display: none;"></canvas>
-								<canvas id="curve-ENCHANTMENT" style="display: none;"></canvas>
-							</div>
-						</div>
-					
-					
-						<div class="col-4 col-t-6">
-							<h4 class="list-stats">General</h4>
-							<ul class="list-stats">
-								<li><span>Author</span> <span class="right"><?php echo($data['author']) ?></span></li>
-								<li><span>Scrolls</span> <span class="right"><?php echo($data['scrolls']) ?></span></li>
-								<li><span>Gold Cost</span> <span class="right"><?php echo($data['cost']) ?>g</span></li>
-								<li><span>Version</span> <span class="right"><?php echo($data['meta']) ?></span></li>
-							</ul>
-						</div>
-						
-						<div class="col-4 col-t-6">
-							<h4 class="list-stats">Types</h4>
-							<div class="col-8">
-								<ul class="list-stats">
-									<?php foreach ($data['types'] as $key => $value) {
-										echo("<li><span><i class='ball-".$key."'></i>".ucfirst(strtolower($key)).":</span> <span class='right'>".$value."</span></li>");
-									} ?>
-									
-								</ul>
-							</div>
-							<div class="col-4">
-								<canvas id="chart-types" width="100px" height="100px" style="margin: 0 auto;"/>
-							</div>
-						</div>
-						
-					</div>
-						
-						
-						
-					<div class="page-header">	
-						<h3>Statistics <small>Specific</small></h3>
-					</div>
-					<div class="col-12">
-						<div class="col-4">
-							<h4 class="list-stats">Sub Types</h4>
-							<ul class="list-stats">
-								<?php foreach ($data['subtype'] as $key => $value) {
-									echo("<li><span>".ucfirst(strtolower($key)).":</span> <span class='right'>".$value."</span></li>");
-								} ?>
-							</ul>
-						</div>
-		
-						<div class="col-4">
-							<h4 class="list-stats">Rarities</h4>
-							<ul class="list-stats">
-								<?php foreach ($data['rarities'] as $key => $value) {
-									
-									switch ($key) {
-										case 0:
-											echo("<li><span>Common:</span> <span class='right'>".$value."</span></li>");
-										break;
-										
-										case 1:
-											echo("<li><span>Uncommon:</span> <span class='right'>".$value."</span></li>");
-										break;
-										
-										case 2:
-											echo("<li><span>Rare:</span> <span class='right'>".$value."</span></li>");
-										break;
-									}
-								
-									
-								} ?>
-							</ul>
-						</div>
+			<div class="page-header">
+				<h3>Statistics <small>General</small></h3>
+			</div>	
+
+			<div class="col-12">
+			<div class="col-12">
+
+			<?php 
+				foreach ($data->curve as $faction => $kind) {
+					foreach ($kind as $name => $cost) { ?>
+						<label class="btn small">
+							<input type="radio" name="curve" value="barChartData<?=$name ?>" data="<?=$name ?>" /> <?=ucfirst(strtolower($name)) ?>
+						</label>
+					<?php }
+				} 
+			?>
+			</div>
+
+				<div class="col-4 col-t-12">
+					<h4 class="list-stats">Curve</h4>
+					<div class="curveContainer">
+						<canvas id="curve-all"></canvas>
+						<canvas id="curve-SPELL" style="display: none;"></canvas>
+						<canvas id="curve-CREATURE" style="display: none;"></canvas>
+						<canvas id="curve-STRUCTURE" style="display: none;"></canvas>
+						<canvas id="curve-ENCHANTMENT" style="display: none;"></canvas>
 					</div>
 				</div>
+
+
+				<div class="col-4 col-t-6">
+					<h4 class="list-stats">General</h4>
+					<ul class="list-stats">
+						<li><span>Author</span> <span class="right"><?=$data->author ?></span></li>
+						<li><span>Scrolls</span> <span class="right"><?=$data->scroll_count ?></span></li>
+						<li><span>Gold Cost</span> <span class="right"><?=$data->total_cost ?>g</span></li>
+						<li><span>Version</span> <span class="right"><?=$data->meta_version ?></span></li>
+					</ul>
+				</div>
+
+				<div class="col-4 col-t-6">
+					<h4 class="list-stats">Types</h4>
+					<div class="col-8">
+						<ul class="list-stats">
+							<?php foreach ($data->kinds as $kind => $count) {
+								echo "<li><span><i class='ball-$kind'></i>".ucfirst(strtolower($kind)).":</span> <span class='right'>$count</span></li>";
+							} ?>
+
+						</ul>
+					</div>
+					<div class="col-4">
+						<canvas id="chart-types" width="100px" height="100px" style="margin: 0 auto;"/>
+					</div>
+				</div>
+
+			</div>
+
+
+
+			<div class="page-header">	
+				<h3>Statistics <small>Specific</small></h3>
+			</div>
+			<div class="col-12">
+				<div class="col-4">
+					<h4 class="list-stats">Sub Types</h4>
+					<ul class="list-stats">
+						<?php foreach ($data->types as $type => $count) {
+							echo "<li><span>".ucfirst(strtolower($type)).":</span> <span class='right'>$count</span></li>";
+						} ?>
+					</ul>
+				</div>
+
+				<div class="col-4">
+					<h4 class="list-stats">Rarities</h4>
+					<ul class="list-stats">
+						<?php foreach ($data->rarities as $rarity => $count) {
+
+							switch ($rarity) {
+								case 0:
+									echo "<li><span>Common:</span> <span class='right'>$count</span></li>";
+								break;
+
+								case 1:
+									echo "<li><span>Uncommon:</span> <span class='right'>$count</span></li>";
+								break;
+
+								case 2:
+									echo "<li><span>Rare:</span> <span class='right'>$count</span></li>";
+								break;
+							}
+
+
+						} ?>
+					</ul>
+				</div>
+			</div>
+		</div>
 		
 		
 		<div class="col-8 col-offset-2">
-			<?php echo($data['text']) ?>
+			<?=$data->text ?>
 		</div>
 		
 		
@@ -192,13 +181,13 @@ $data = $deck->get_deck_data($base->get_var(1));
 	var ENCHANTMENT = "#90b1a6";
 	
 	var pieData = [
-			<?php foreach ($data['types'] as $key => $value) { ?>
+			<?php foreach ($data->kinds as $kind => $count) { ?>
 				
 				{
-					value: <?php echo($value) ?>,
-					color: <?php echo($key) ?>,
-					highlight: <?php echo($key) ?>,
-					label: "<?php echo(ucfirst(strtolower($key))) ?>"
+					value: <?=$count ?>,
+					color: <?=$kind ?>,
+					highlight: <?=$kind ?>,
+					label: "<?=ucfirst(strtolower($kind)) ?>"
 				},
 			<?php } ?>
 			
@@ -212,20 +201,18 @@ $data = $deck->get_deck_data($base->get_var(1));
 	
 	
 	<?php
-		foreach ($data['curve'] as $faction => $value) {
-			foreach ($data['curve'][$faction] as $type => $value) { ?>
+		foreach ($data->curve as $faction => $kind) {
+			foreach ($kind as $name => $cost) { ?>
 				
-				var barChartData<?php echo($type) ?> = {
+				var barChartData<?=$name ?> = {
 					labels : [1,2,3,4,5,6,7,8,9],
 					datasets : [
 						{
-							label : "<?php echo($faction." ".$type) ?>",
-							fillColor : <?php echo($faction) ?>,
-							highlightFill: dark_<?php echo($faction) ?>,
+							label : "<?="$faction $name" ?>",
+							fillColor : <?=$faction ?>,
+							highlightFill: dark_<?=$faction ?>,
 							data : [
-								<?php foreach ($data['curve'][$faction][$type] as $key => $value) {
-									echo($value.",");
-								} ?>
+								<?=implode(',',$cost) ?>
 							]
 						},
 						],
@@ -238,10 +225,11 @@ $data = $deck->get_deck_data($base->get_var(1));
 	
 		window.onload = function(){
 			<?php
-				foreach ($data['curve'] as $faction => $value) {
-					foreach ($data['curve'][$faction] as $type => $value) { ?>
-						loadGraph("curve-<?php echo($type) ?>", barChartData<?php echo($type) ?>);
-					<?php }
+				foreach ($data->curve as $faction => $kind) {
+					foreach ($kind as $cost => $value) {
+			?>
+				loadGraph("curve-<?=$cost ?>", barChartData<?=$cost ?>);
+			<?php		}
 				}
 			?>
 			
