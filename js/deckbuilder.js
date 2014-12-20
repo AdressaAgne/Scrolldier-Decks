0fdbc71f69a4fdb93b3678e5c280bff8b3b72f7a
OrangeeWeb <agne240@me.com>
2014-12-20 14:53:20 +0100

    Start on Deckbuilder 2.0

---
+++ js/deckbuilder.js
@@ -0,0 +1,88 @@
+$(function() {
+
+	var allScrolls = [];
+	
+	$("[id=addScrollScroll]").click(function() {
+		var	id = $(this).attr("data-id");
+		var image = $(this).attr("data-image");
+		var faction = $(this).attr("data-faction");
+		var name = $(this).attr("data-name");
+		
+		addScroll(parseInt(id), parseInt(image), faction, name);
+	});
+	
+	
+	
+	$("#deck-scrolls").on("click", "div[data-id]",function() {
+		console.log("Removing Scroll");
+		removeScroll($(this).attr("data-id"));
+	});
+	
+	
+	
+	$(".scroll-front").hover(function() {
+		changePreview($(this).attr("data-name"));
+	});
+	
+	function changePreview(name) {
+		console.log("changing preview to "+name);
+		$("#previewImage").attr("src", "http://a.scrollsguide.com/image/screen?name="+name+"&size=large");
+	}
+	
+	function getHTML(id, image, faction, name, count) {
+		return '<div class="col-2 col-phone-6 col-tab-3 hand scrolls" data-name="'+name+'" data-id="'+id+'" data-count="'+count+'" data-faction="'+faction+'" data-image="'+image+'">
+				<div class="col-12 scroll scroll-stack-'+count+'" style="background-image: url(/img/scrolls/'+image+'.png)">
+					<i class="icon-'+faction+'"></i>
+				</div>
+				<div class="col-12 scroll-content">
+					<span id="count">'+count+'x</span> <span id="name">'+name+'</span>
+				</div>
+			</div>';
+	}
+	
+	function getInnerHTML(id, image, faction, name, count) {
+		return '<div class="col-12 scroll scroll-stack-'+count+'" style="background-image: url(/img/scrolls/'+image+'.png)">
+					<i class="icon-'+faction+'"></i>
+				</div>
+				<div class="col-12 scroll-content">
+					<span id="count">'+count+'x</span> <span id="name">'+name+'</span>
+				</div>';
+	}
+	
+	function addScroll(id, image, faction, name) {
+		if ($("#deck-scrolls [data-id="+id+"]").length) {
+			var scrollID = $("#deck-scrolls").find("[data-id="+id+"]");
+			var dataCount = $(scrollID).attr("data-count");
+			var newCount = parseInt(dataCount)+1;
+			
+			if (dataCount != 3) {
+				console.log("Adding 1 scroll to "+name+"("+(newCount)+")");
+				$(scrollID).html(getInnerHTML(id, image, faction, name, newCount));
+				$(scrollID).attr("data-count", newCount);
+			} else{
+				console.log("Already got 3 scrolls of "+name);
+			}
+			
+		} else {
+			$("#deck-scrolls").append(getHTML(id, image, faction, name, 1));
+		}
+	}
+	
+	function removeScroll(id) {
+		var scrollID = $("#deck-scrolls").find("[data-id="+id+"]");
+		var dataCount = $(scrollID).attr("data-count");
+		var name = $(scrollID).find("#name").text();
+		var faction = $(scrollID).attr("data-faction");
+		var image = $(scrollID).attr("data-image");
+		
+		var newCount = parseInt(dataCount)-1;
+		
+		if (dataCount != 1) {
+			console.log("Adding 1 scroll to "+name+"("+(newCount)+")");
+			$(scrollID).html(getInnerHTML(id, image, faction, name, newCount));
+			$(scrollID).attr("data-count", newCount);
+		} else{
+			$(scrollID).remove();
+		}
+	}
+});
--- view/deck/deckbuilder.php
+++ view/deck/deckbuilder.php
@@ -1,5 +1,110 @@
+<style>
+	.library-scrolls{
+		height: 400px;
+		overflow: scroll;
+	}
+</style>
 <div class="container">
 	<div class="page-header">
-		<h2>DeckBuilder</h2>
+		<h2 id="test">Deck Builder</h2>
+	</div>
+	<div class="col-12">
+		<div class="col-4">
+			<div class="form-element">
+				<input type="text" name="" value="" placeholder="Search" />
+			</div>
+		</div>
+		<div class="col-4">
+			<div class="form-element">
+				<label class="icon">
+					<input type="checkbox" name="faction" value="growth" /> <span><i class="icon-growth"></i></span>
+				</label>
+				<label class="icon">
+					<input type="checkbox" name="faction" value="order" /> <span><i class="icon-order"></i></span>
+				</label>
+				<label class="icon">
+					<input type="checkbox" name="faction" value="energy" /> <span><i class="icon-energy"></i></span>
+				</label>
+				<label class="icon">
+					<input type="checkbox" name="faction" value="decay" /> <span><i class="icon-decay"></i></span>
+				</label>
+			</div>
+		</div>
 	</div>
 </div>
+<div class="container-fluid">
+	<div class="row">
+	
+		<div class="col-2">
+			<img id="previewImage" src="http://a.scrollsguide.com/image/screen?name=Blessing of Haste&size=large" alt="" />
+			<!--<div class="col-12 align-center">
+				<h2><i class="icon-growth"></i>Scrolls name <br /><small>Spell: Countdown</small></h2>
+			</div>
+			<div class="col-12 deck-scrolls">
+				 <div class="scroll" style="background-image: url('/img/scrolls/490.png');"></div>
+			</div>
+			<div class="col-12">
+				ap: 3, cd:4 ,hp: 4 
+			</div>
+			<div class="col-12">
+				Description
+			</div>
+			<div class="col-12 align-center">
+				<i>Falvor</i>
+			</div>-->
+		</div>
+		
+		<div class="col-8 deck-scrolls" id="deck-scrolls"></div>
+	
+		<div class="col-2 deck-scrolls library-scrolls">
+				<?php
+				$query = $deck->_db->prepare("SELECT * FROM scrollsCard ORDER BY costGrowth DESC, costEnergy DESC, costDecay DESC, costOrder DESC");
+				$query->execute();
+				while ($row = $query->fetch(PDO::FETCH_ASSOC)) { 
+					$cost = 0;
+					$faction = "";
+					
+					if (!empty($row['costorder'])) {
+							
+						$cost = $row['costorder'];
+						$faction = "order";
+						
+					}
+					if (!empty($row['costgrowth'])) {
+					
+						$cost = $row['costgrowth'];	
+						$faction = "growth";
+						
+					}
+					if (!empty($row['costenergy'])) {
+					
+						$cost = $row['costenergy'];
+						$faction = "energy";
+					
+					}
+					if (!empty($row['costdecay'])) {
+					
+						$cost = $row['costdecay'];
+						$faction = "decay";
+					}
+				
+				?>
+				<div class="col-12 scroll-front hand" id="addScrollScroll"
+					 data-faction="<?= $faction ?>"
+					 data-name="<?=$row['name']?>"
+					 data-id="<?=$row['id']?>"
+					 data-image="<?=$row['image']?>">
+					 <div class=" scroll" style="background-image: url('/img/scrolls/<?=$row['image']?>.png');">
+						
+						<div class="col-12 text">
+							<?=$cost ?> <i class="icon-<?= $faction ?> front-text"></i><?=$row['name'] ?>
+						</div>
+					</div>
+					
+				</div>
+				<?php } ?>
+		</div>
+	</div>
+</div>
+
+<script src="/js/min/deckbuilder-min.js"></script>
